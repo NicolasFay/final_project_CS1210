56cf8e4f4a20e8bac7ed5db9dbe24fcdd4184a2e
Joe <jdonova9@uvm.edu>
2023-11-28 15:20:29 -0500

    Create deck of cards

--- final_project.py
+++ final_project.py
@@ -5,4 +5,18 @@ CS 1210
 11/2/2023
 '''
 
+import random
+
 # yo
+
+RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+SUITS = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+
+
+if __name__ == "__main__":
+    deck = []
+    for i in RANKS:
+        for e in SUITS:
+            deck.append(f"{i} of {e}")
+    random.shuffle(deck)
+    print(deck)
