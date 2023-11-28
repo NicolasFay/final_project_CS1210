392d80ed7823ddee4b4c8e92b0c108c6bc9207f5
Joe <jdonova9@uvm.edu>
2023-11-28 16:50:43 -0500

    Section off parts of program to functions

--- final_project.py
+++ final_project.py
@@ -14,12 +14,50 @@ import random
 
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
 SUITS = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+CARD_VALUES = {}
 
 
-if __name__ == "__main__":
+def createdeck(ranks, suits):
     deck = []
     for i in RANKS:
         for e in SUITS:
             deck.append(f"{i} of {e}")
+    num = 2
+    count = 0
+    for cardName in deck:
+        CARD_VALUES[cardName] = num
+        count += 1
+        if count == 4:
+            num += 1
+            count = 0
     random.shuffle(deck)
-    print(deck)
+    return deck
+
+
+def deal(deck):
+    stack1 = []
+    stack2 = []
+    while deck:
+        stack1.append(deck.pop(-1))
+        stack2.append(deck.pop(-1))
+    return stack1, stack2
+
+
+if __name__ == "__main__":
+    stack1, stack2 = deal(createdeck(RANKS, SUITS))
+    print(stack1)
+    print(stack2)
+
+    while stack1 and stack2:
+        print(f"{stack1[-1]} VS {stack2[-1]}")
+        if CARD_VALUES[stack1[-1]] > CARD_VALUES[stack2[-1]]:
+            stack1.insert(0, stack2.pop(-1))
+        elif CARD_VALUES[stack1[-1]] < CARD_VALUES[stack2[-1]]:
+            stack2.insert(0, stack1.pop(-1))
+        else:
+            break
+    print(stack1)
+    print(stack2)
+
+
+
